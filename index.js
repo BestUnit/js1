cdc02e00e401c9fc515b6d2a0c7550256fe18727
BestUnit <bestunit52rus@gmail.com>
2025-09-30 20:05:53 +0300

    Инициализировали проект

---
+++ index.js
@@ -0,0 +1,83 @@
+// 1.Вывести в терминал строку “Hello World!”
+
+console.log ("Hello World!!!");
+
+// 2. Создать переменную для каждого типа данных
+
+const age = 34;
+
+let userName = 'Evgeniy';
+
+let isAdmin = false;
+
+const fruit = null;
+
+let height;
+
+const user = {
+    name: 'Sergey', 
+    age: 30,
+};
+
+const id = Symbol('id');
+
+const check = 12313434545221222423423424n;
+
+// 3. Вывести в консоль редактора кода созданные переменные
+
+console.log (age);
+
+console.log (userName);
+
+console.log (isAdmin);
+
+console.log (fruit);
+
+console.log (height);
+
+console.log (user);
+
+console.log (id);
+
+console.log (check);
+
+//4. Вывести ее тип через оператор typeof и функцию typeof
+
+console.log (typeof age);
+
+console.log (typeof (userName));
+
+console.log (typeof isAdmin);
+
+console.log (typeof (fruit));
+
+console.log (typeof height);
+
+console.log (typeof (user));
+
+console.log (typeof id);
+
+console.log (typeof (check));
+
+//5. Попробовать изменить переменную объявленную через const
+
+age=35
+
+//6. Попробовать изменить переменную с типом object объявленную через const
+
+user = {
+    name: Oleg
+}
+
+
+//7. Попробовать изменить переменную с типом object объявленную через let 
+
+userName = 'Gosha';
+
+console.log (userName)
+
+//8. Попробовать изменить переменную с типом object объявленную через var 
+
+var car = "Audi"
+car = "BMW"
+console.log (car)
